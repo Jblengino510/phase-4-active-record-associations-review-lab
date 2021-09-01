0367b82e3ff593c95a18164f2e28c7f07900b1a1
Joshua <j.blengino@yahoo.com>
2021-09-01 10:02:51 -0700

    commit tree a222eb8b069226c39ab96b73f8d8ec114beb70b2

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.belongs_to :passenger, foreign_key: true, null: false
+      t.belongs_to :taxi, foreign_key: true, null: false
       t.timestamps
     end
   end
